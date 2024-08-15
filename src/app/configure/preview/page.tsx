04ddabeee2dc8c9818398f19cf8c228316528183
Yousuf <yousufali06296@gmail.com>
2024-08-15 08:48:12 +0530

    reinitialized files

---
+++ src/app/configure/preview/page.tsx
@@ -0,0 +1,27 @@
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+import DesignPreview from "./DesignPreview";
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  return <DesignPreview configuration={configuration} />;
+};
+
+export default Page;
